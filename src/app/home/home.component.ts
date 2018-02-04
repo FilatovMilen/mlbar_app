6019c57372824a955565684b5e9714e514089627
Milen Filatov <filatov.milen@gmail.com>
2018-02-04 04:24:48 +0000

    Push random stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'home',
@@ -6,4 +7,14 @@ import {Component} from '@angular/core';
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+    router: Router;
+
+    constructor(_router: Router) {
+        this.router = _router;
+    }
+
+    navigateToRecipeGen() {
+        this.router.navigateByUrl('/recipe');
+    }
 }
